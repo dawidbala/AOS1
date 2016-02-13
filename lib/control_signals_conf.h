4b931dae73c47fc14be1126ed830bfacb3865556
dawidbala <balka.pc@gmail.com>
2016-02-13 03:12:27 +0100

    Test

---
+++ lib/control_signals_conf.h
@@ -0,0 +1,19 @@
+// Buzzer
+#define BUZ                 PC6
+
+// Czewona dioda (workLed)
+#define wLED                PC7
+#define wLED_DIR            DDRC
+#define wLED_PORT           PORTC
+
+// Przyciski
+#define Btn_DIR             DDRD
+#define Btn_PORT            PORTD
+#define Btn_LeftMinus       0x7F
+#define Btn_LeftPlus        0xBF
+#define Btn_PS              0xDF
+#define Btn_S               0xEF
+#define Btn_M               0xF7
+#define Btn_Remote          0xFB
+#define Btn_RightMinus      0xFD
+#define Btn_RightPlus       0xFE
